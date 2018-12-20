a69510ab0446e3e0a3c7818061b942628ab311f9
zhaoheqiang <zhaoheqiang@zhaoheqiangdeMacBook-Air.local>
2018-12-20 10:46:09 +0800

    init_project

---
+++ src/main/java/com/zhq/sqlmonitor/SqlmonitorApplication.java
@@ -0,0 +1,14 @@
+package com.zhq.sqlmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlmonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlmonitorApplication.class, args);
+    }
+
+}
+
